12aa72e65071e734b9431735dbc1d43f4fdf8b02
Thotheolh <twzgerald@gmail.com>
2015-08-25 18:08:24 +0800

    Created testing server skeletal codes.

--- src/php/wspp-server/WsppServer.php
+++ src/php/wspp-server/WsppServer.php
@@ -1,5 +1,17 @@
 <?php
+use Ratchet\Server\IoServer;
+use Chat;
 
-echo "Hello World";
+$port = 8080;
+
+echo "Loading required framework...";
+require __DIR__ . '/../../../vendor/autoload.php';
+
+echo "Starting WSPP Server...\r\n";
+$server = IoServer::factory(new Chat(), $port);
+
+echo "Running Server...\r\n";
+$server->run();
+
 
 ?>
