20f85f9185c1f4c31660c299609b046a1879c7cc
geraldtay <gerald@SNC-PS-003.corp.sunnic>
2015-08-25 15:02:43 +0800

    Skeleton codes with no protocol yet.

---
+++ src/web/contacts.js
@@ -0,0 +1,68 @@
+/*
+ * WEB-SECURE PUSH PROTOCOL CHAT (WSPPCHAT).
+ * A Secure Push Protocol Chat System.
+ *
+ * Copyright (C) 2015  Thotheolh
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/*
+ * Adds a new contact
+ */
+function add_contact(pubkey) {
+}
+
+/*
+ * Removes an existing contact
+ */
+function remove_contact(pubkey) {
+}
+
+/*
+ * Blocks an existing contact
+ */
+function block_contact(pubkey) {
+}
+
+/*
+ * List your contacts
+ */
+function list_contact() {
+}
+
+/*
+ * Gets a contact's public details from your contact list
+ */
+function get_contact(pubkey) {
+}
+
+/*
+ * Creates a named group chat
+ */
+function make_group(grp_name) {
+}
+
+/*
+ * Invite an existing contact to a group chat. Invitee must be from any group member's existing contacts list.
+ */
+function invite_group(pubkey, grp_name) {
+}
+
+/*
+ * Leaves an existing named group.
+ */
+function leave_group(grp_name) {
+}
